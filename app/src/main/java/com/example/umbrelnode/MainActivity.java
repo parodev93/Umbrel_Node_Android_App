e5aff633dac3173c98b320efd2bf2cd679d99455
paro272200 <patrick.roth@elektrobit.com>
2022-07-14 19:15:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/umbrelnode/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.umbrelnode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+    private ProgressDialog progressDialog;
+    private boolean error;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+
+        myWebView = (WebView) findViewById(R.id.webview);
+        final Activity activity = this;
+        // Enable JavaScript and lets the browser go back
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        //myWebView.canGoBack();
+        myWebView.setWebViewClient(new WebViewClient() {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
+                Toast.makeText(activity, error.getDescription(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        // The URL that webview is loading
+        myWebView.loadUrl("http://umbrel.local");
+
+    }
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
