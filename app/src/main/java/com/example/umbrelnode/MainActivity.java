2a5fbbefe12ff7e6c8563056f1819b09dd2a5750
parodev93 <paddy.ro@outlook.de>
2022-07-15 10:30:30 +0200

    Implementation of SwipeRefreshLayout and WebChromeClient

--- app/src/main/java/com/example/umbrelnode/MainActivity.java
+++ app/src/main/java/com/example/umbrelnode/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.umbrelnode;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.Window;
+import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -15,6 +17,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private WebView myWebView;
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,34 +25,46 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
+        myWebView = findViewById(R.id.webview);
+        swipeRefreshLayout = findViewById(R.id.swiperefreshlayout);
+        swipeRefreshLayout.setOnRefreshListener(() -> myWebView.reload());
+        LoadWeb();
+    }
+    @SuppressLint("SetJavaScriptEnabled")
+    public void LoadWeb() {
 
-        myWebView = (WebView) findViewById(R.id.webview);
         final Activity activity = this;
         myWebView.getSettings().setJavaScriptEnabled(true);
         myWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.setWebChromeClient(new WebChromeClient());
         myWebView.setWebViewClient(new WebViewClient() {
+            @SuppressWarnings("deprecation")
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
 
-          /*  @Override
+            @Override
             public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error){
                 Toast.makeText(activity, error.getDescription(), Toast.LENGTH_LONG).show();
-            }*/
+            }
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+            }
         });
-
         // The URL that webview is loading
         myWebView.loadUrl("http://umbrel.local");
-
+        swipeRefreshLayout.setRefreshing(true);
     }
+
     @Override
     public void onBackPressed() {
         if (myWebView.canGoBack()) {
             myWebView.goBack();
         } else {
-            super.onBackPressed();
+            finish();
         }
     }
 }
